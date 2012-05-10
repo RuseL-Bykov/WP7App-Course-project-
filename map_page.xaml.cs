9b763daad38cac366f7cd20fd769675386748700
Rusel Bykov <Rusel14@gmail.com>
2012-05-10 18:41:38 +0300

    Оптимизирована работа карты, ставлено определение координат.

--- map_page.xaml.cs
+++ map_page.xaml.cs
@@ -15,6 +15,7 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Controls.Maps;
 using Microsoft.Devices.Sensors;
 using Microsoft.Xna.Framework;
+using System.Threading;
 
 namespace MapForCurs
 {
@@ -23,23 +24,58 @@ namespace MapForCurs
         private Accelerometer myAccel;
         private GeoCoordinateWatcher myGeoWatcher;
         private Vector3 currentValues;
-        public map_page()
+        private Pushpin myPushpin;
+
+        public map_page() //конструктор
         {
+
             InitializeComponent();
 
-            myAccel = new Accelerometer();
-            myAccel.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<AccelerometerReading>>(myAccel_CurrentValueChanged);
-            myAccel.Start();
-            myGeoWatcher = new GeoCoordinateWatcher();
+
+            myGeoWatcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
             myGeoWatcher.MovementThreshold = 100.0f;
 
-            // myGeoWatcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(myGeoWatcher_StatusChanged);
+            myGeoWatcher.StatusChanged += myGeoWatcher_StatusChanged;
 
-            myGeoWatcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(myGeoWatcher_PositionChanged);
-            myGeoWatcher.TryStart(false, TimeSpan.FromSeconds(60));
+            myGeoWatcher.PositionChanged += myGeoWatcher_PositionChanged;
+            myAccel = new Accelerometer();
+            myAccel.CurrentValueChanged += myAccel_CurrentValueChanged;
+            myAccel.Start();
+            myPushpin = new Pushpin();
+           myGeoWatcher.TryStart(false, TimeSpan.FromSeconds(60));
         }
 
-        void myAccel_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)
+
+        void myGeoWatcher_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e) //обработчик сервися определения местоположений
+        {
+            switch (e.Status)
+            {
+                case GeoPositionStatus.Disabled:
+                    if (myGeoWatcher.Permission == GeoPositionPermission.Denied)
+                    {
+                        GeoStatus.Text = "Сервис выключен";
+                    }
+                    else
+                    {
+                        GeoStatus.Text = "На этом устройстве сервис недоступен";
+                    }
+                    break;
+                case GeoPositionStatus.Initializing:
+                    GeoStatus.Text = "Сервис инициализируется";
+                    break;
+                case GeoPositionStatus.NoData:
+                    GeoStatus.Text = "Данные о месположении недоступны";
+                    break;
+                case GeoPositionStatus.Ready:
+                    {
+                        GeoStatus.Text = "Данные о местоположении доступны";
+                    }
+                    break;
+            }
+
+        }
+
+        void myAccel_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)  //работа акселерометра
         {
             if (myAccel.IsDataValid)
             {
@@ -62,32 +98,35 @@ namespace MapForCurs
 
         }
 
-        private void HandleZoomIn()
+        private void HandleZoomIn() //акселерометр +
         {
             MyMap.ZoomLevel += 1;
         }
 
-        private void HandleZoomOut()
+        private void HandleZoomOut() //акселерометр -
         {
             MyMap.ZoomLevel -= 1;
         }
 
-        void myGeoWatcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        void myGeoWatcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)  //обработчик смены позиции
         {
             MyMap.Center = e.Position.Location;
+            myPushpin.Location = e.Position.Location;
+            if (!MyMap.Children.Contains(myPushpin)) MyMap.Children.Add(myPushpin);
+            myPushpin.Content = "Ваши GPS координаты:\n" + myGeoWatcher.Position.Location.Latitude.ToString() + "\n" + myGeoWatcher.Position.Location.Longitude.ToString();
         }
 
-        private void ZoomIn_Click(object sender, EventArgs eventArgs)
+        private void ZoomIn_Click(object sender, EventArgs eventArgs) // увеличиваем пасштаб
         {
             MyMap.ZoomLevel += 1;
         }
 
-        private void ZoomOut_Click(object sender, EventArgs eventArgs)
+        private void ZoomOut_Click(object sender, EventArgs eventArgs) //уменшаем масшатб
         {
             MyMap.ZoomLevel -= 1;
         }
 
-        private void LayoutChange_Click(object sender, EventArgs eventArgs)
+        private void LayoutChange_Click(object sender, EventArgs eventArgs) //изменение вида карты
         {
             if (MyMap.Mode is RoadMode)
             {
@@ -100,7 +139,7 @@ namespace MapForCurs
 
         }
 
-        private  void BackToMenu_Click(object sender, EventArgs eventArgs)
+        private  void BackToMenu_Click(object sender, EventArgs eventArgs) //кнопка назад в меню
         {
             NavigationService.GoBack();
         }
